e943d3bfecf8e44a8b620879de9ad89bda310c64
kei10007 <kei1994forgit@gmail.com>
2023-11-08 23:41:28 +0900

    tweak

--- conf.py
+++ conf.py
@@ -14,7 +14,10 @@ release = 'v0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+  'sphinx_rtd_theme',
+  'sphinx.ext.githubpages'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
